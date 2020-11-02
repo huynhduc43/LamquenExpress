26cc2527e85ad17af9d3459d10dd2d350ca656ef
Đức Huỳnh Thanh <thanhduc4320@gmail.com>
2020-11-02 14:42:44 +0700

    Modify index.hbs & styles.css

--- betaptinh/routes/index.js
+++ betaptinh/routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Bé tập tính' });
+});
+
+router.post('/', (req, res)=> {
+  const number = req.body.number;
+  const result = Math.pow(parseFloat(number),2);
+  res.render('index', { title: 'Bé tập tính', number, result });
 });
 
 module.exports = router;
